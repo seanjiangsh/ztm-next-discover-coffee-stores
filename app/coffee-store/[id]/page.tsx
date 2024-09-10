fc608359081e0bae96b8eec9c58e03ef4d183b35
sean <seanjiangsh@gmail.com>
2024-09-11 06:24:40 +0800

    feat: styling coffee store page and generate static params for SSG

--- app/coffee-store/[id]/page.tsx
+++ app/coffee-store/[id]/page.tsx
@@ -1,7 +1,15 @@
-import React from "react";
 import Link from "next/link";
+import Image from "next/image";
 
-import { fetchCoffeeStore } from "@/lib/coffee-stores";
+import { fetchCoffeeStore, fetchCoffeeStores } from "@/lib/coffee-stores";
+
+const altImgUrl =
+  "https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80";
+
+export async function generateStaticParams() {
+  const coffeeStores = await fetchCoffeeStores();
+  return coffeeStores.map(({ id }) => ({ id }));
+}
 
 type PageProps = {
   params: { id: string };
@@ -10,13 +18,40 @@ type PageProps = {
 export default async function Page(props: PageProps) {
   const { id } = props.params;
   const coffeeStore = await fetchCoffeeStore(id);
-  console.log(coffeeStore);
+  const { name = "", address = "", imgUrl = altImgUrl } = coffeeStore || {};
+  // console.log(coffeeStore);
 
   return (
     <div className="h-full pb-80">
       <div className="m-auto grid max-w-full px-12 py-12 lg:max-w-6xl lg:grid-cols-2 lg:gap-4">
-        <div className="mb-2 mt-24 text-lg font-bold">
-          <Link href="/">← Back to Home</Link>
+        <div className="">
+          <div className="mb-2 mt-24 text-lg font-bold">
+            <Link href="/">← Back to home</Link>
+          </div>
+          <div className="my-4">
+            <h1 className="text-4xl">{name}</h1>
+          </div>
+          <Image
+            src={imgUrl}
+            width={740}
+            height={360}
+            className="max-h-[420px] min-w-full max-w-full rounded-lg border-2 sepia lg:max-w-[470px] "
+            alt={"Coffee Store Image"}
+          />
+        </div>
+
+        <div className={`glass mt-12 flex-col rounded-lg p-4 lg:mt-48`}>
+          {address && (
+            <div className="mb-4 flex">
+              <Image
+                src="/static/icons/places.svg"
+                width="24"
+                height="24"
+                alt="places icon"
+              />
+              <p className="pl-2">{address}</p>
+            </div>
+          )}
         </div>
       </div>
     </div>
