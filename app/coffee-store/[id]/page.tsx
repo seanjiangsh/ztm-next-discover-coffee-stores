44e1ee18bddaaa2882925d81cf87cddda95dd024
Sean Jiang <seanjiangsh@gmail.com>
2024-09-25 06:40:04 +0800

    feat: added upvote button loading state with useFormStatus

--- app/coffee-store/[id]/page.tsx
+++ app/coffee-store/[id]/page.tsx
@@ -38,7 +38,7 @@ export default async function Page(props: PageProps) {
     <div className="h-full pb-80">
       <div className="m-auto grid max-w-full px-12 py-12 lg:max-w-6xl lg:grid-cols-2 lg:gap-4">
         <div className="">
-          <div className="mb-2 mt-24 text-lg font-bold">
+          <div className="mb-2 lg:mt-24 text-lg font-bold">
             <Link href="/">← Back to home</Link>
           </div>
           <div className="my-4">
--- components/upvote.client.tsx
+++ components/upvote.client.tsx
@@ -1,10 +1,38 @@
 "use client";
 import Image from "next/image";
-import { useFormState } from "react-dom";
+import { useFormState, useFormStatus } from "react-dom";
 
 import { upvoteAction } from "@/actions";
 
 type UpvoteProps = { id: string; voting: number };
+
+// * note: The useFormStatus can only be used as a child
+// * of a form element using Server Action.
+
+export const SubmitButton = () => {
+  const { pending } = useFormStatus();
+  const buttonContent = pending ? (
+    <Image
+      src="/static/icons/loading-spinner.svg"
+      width="30"
+      height="30"
+      alt="Loading"
+      className="m-auto animate-spin"
+    />
+  ) : (
+    "Up vote!"
+  );
+  return (
+    <button
+      type="submit"
+      disabled={pending}
+      aria-disabled={pending}
+      className="bg-purple-951 min-w-[120px] h-[60px]"
+    >
+      {buttonContent}
+    </button>
+  );
+};
 
 export default function Upvote(props: UpvoteProps) {
   const [state, dispatch] = useFormState(upvoteAction, props);
@@ -21,7 +49,7 @@ export default function Upvote(props: UpvoteProps) {
         />
         <p className="pl-2">{voting}</p>
       </div>
-      <button type="submit">Up vote!</button>
+      <SubmitButton />
     </form>
   );
 }
